1c8a0c91f6e98a42ce03e989446b33d362fb0775
nandeesh neeli <nandeesh.neeli@gmail.com>
2021-03-18 16:25:32 +0530

    changes in login-modal.service releated to login popup

--- src/main/webapp/app/core/login/login-modal.service.ts
+++ src/main/webapp/app/core/login/login-modal.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { LoginModalComponent } from '../../shared/login/login.component';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class LoginModalService {
   private isOpen = false;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private router: Router) {}
 
   open(): void {
-    if (this.isOpen) {
+    this.router.navigate(['']);
+    /*  if (this.isOpen) {
       return;
     }
     this.isOpen = true;
     const modalRef: NgbModalRef = this.modalService.open(LoginModalComponent);
-    modalRef.result.finally(() => (this.isOpen = false));
+    modalRef.result.finally(() => (this.isOpen = false));*/
   }
 }
